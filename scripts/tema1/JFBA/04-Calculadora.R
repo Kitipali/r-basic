b89763e3a921aa9ba14bb2f4589f2301355b1fe0
Kitipali <srmjfba@gmail.com>
2025-02-25 10:22:37 +0100

    Ejercicios

---
+++ scripts/tema1/JFBA/04-Calculadora.R
@@ -0,0 +1,117 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2%/%3+5
+2%%3+5
+2^-4
+
+725%/%7
+
+725%%7
+
+725/7
+7*0.5714
+
+pi
+2*pi
+Inf
+-Inf
+
+NA
+
+NaN
+
+5/0
+0/0
+help(choose)
+
+
+sin(pi/2)
+cos(pi)
+tan(0)
+
+cos(60*pi/180) 
+
+?sin
+
+tan(pi)
+tan(pi/2)
+
+sqrt(2)
+print(sqrt(2),10)
+round(sqrt(2),3)
+floor(sqrt(2))
+ceiling(sqrt(2))
+trunc(sqrt(2))
+sqrt(2)^2-2
+2^50
+print(2^50,15)
+print(pi,16)
+x = (pi^2)/2
+x
+
+y <- cos(pi/4)
+y
+
+sin(pi/4) + cos(pi/4) -> z
+z
+
+edad <- 30
+nombre = "Juan Gabriel"
+HOLA = 1
+hola = 5
+x= 2
+x = x^2
+f = function(x) { x^3 - (3^x)* sin(x)}
+f(4)
+f = function(x) { 
+  x^3 - (3^x)* sin(x)
+  }
+f(5)
+
+suma1 <- function(t) {
+  t + 1
+}
+suma1(2)
+
+product <- function(x,y) {
+  x*y
+}
+product(5,7)
+
+g<- function (x,y,z) {
+  exp(x^2 +y^2)* sin(z)
+}
+g(1,2,3)
+g(1,-1, pi)
+
+suma5 <- function(numero){
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero
+}
+
+suma5(3)
+
+suma5 <- function(numero){
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  suma1(numero);
+}
+
+suma5(3)
+
+ls()
+rm(product)
+ls()
+
+rm(list = ls())
+
+
+
